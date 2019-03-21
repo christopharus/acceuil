582ff0f5baa8a620200748c5661f8e7716ce8414
christopharus <47279703+christopharus@users.noreply.github.com>
2019-03-21 16:14:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,67 @@
 	<html>
 		<head>
 			<meta charset="utf-8" />
+			<link rel="stylesheet" href="css/bootstrap/dist/css/bootstrap.min.css" />
+			<!--En effet il faut deux feuilles de styles celle de bootstrap et une autre pour notre propre design -->
+			<link rel="stylesheet" href="css/style.css" />
+			
 			<title>Acceuil</title>
 		</head>
 		<body>
-		
-			<h1>C'est bien cool</h1>	
+			<!--Creation d'une entete pour la bar et le menu -->
+			<header class="container-fluid" >
+				<div class="menu">
+						<nav class="navi">
+							<a>Acceuil</a>
+							<a>About</a>
+							<a>Forum</a>
+					</nav>
+				</div>
+			</header>
+			
+			<!--On peut aussi creer une section pour une banniere pour placer une image -->
+			<section class="container-fluid  banner ">
+				<div class="ban">
+					<img src="source de l'image " alt="image de la banniere ">
+				</div>
+			
+			</section>
+				
+				
+			</section>
+			<!--Creation d'une portion pour le texte a saisir avec 4 articles la conception est basee sur bootstrap du coup on divise 12/4 et on a 3 pour les grands ecrans et 12 pour les petits ecrans -->
+			<section class="container-fluid">
+				<h1>Titre de la portion</h1>
+					<article class="col-xm-12 col-sm-12 col-md-3 col-lg-3">
+						
+				</article>
+				<article class="col-xm-12 col-sm-12 col-md-3 col-lg-3">
+						
+				</article>
+				<article class="col-xm-12 col-sm-12 col-md-3 col-lg-3">
+						
+				</article>
+				<article class="col-xm-12 col-sm-12 col-md-3 col-lg-3">
+						
+				</article>
+				
+			</section>
+			<!--A partir de la le site est presque fini sa dependras de votre facon de voir les choses maintenant -->
+			
+			<!--Conception du footer ou pied de page -->
+			<footer class="container-fluid">
+				<!--On peut soit utiliser des liens soit juste des paragraphes-->
+				<a>Quelque chose</a>
+				<a>Autre chose</a>
+				<a>Copyright Christopharus</a>
+			</footer>
+		</body>
+</html>
+			
+			
+			
+			
+			
 
 
 		</body>
